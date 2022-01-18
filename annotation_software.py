3edbd1826242111df46046c78d1a89ea5bd9d692
Yu-ChengDutch <39351818+Yu-ChengDutch@users.noreply.github.com>
2022-01-19 00:25:45 +0100

    Finished writing the program, but now in PyQT

--- annotation_software.py
+++ annotation_software.py
@@ -1,26 +1,20 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import matplotlib.gridspec as gridspec
 from matplotlib.widgets import Button
+from matplotlib.widgets import RadioButtons
 from matplotlib.backend_bases import MouseButton
 from matplotlib.patches import Rectangle
 import numpy as np
 import pandas as pd
 
-base_path = "C:/Users/zmezl/Desktop/University/AI/Thesis/eXAI_thesis/"
-path = base_path + "Amin_dataset/"
 
-fig, ax = plt.subplots()
+def initialize():
+    plt.suptitle("Right = lesion \n left = background")
+    plt.subplots_adjust(bottom=0.2)
+    plt.xticks(ticks=x)
+    plt.yticks(ticks=x)
 
-plt.suptitle("Right = lesion \n left = background")
-
-plt.subplots_adjust(bottom=0.2)
-x = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
-plt.xticks(ticks=x)
-plt.yticks(ticks=x)
-
-reg_array = np.zeros((10,10))
-
-wb = pd.read_excel(base_path + "fitzpatrick17k-amin-annotation.xlsx")  
 
 class Index:
     
@@ -41,18 +35,18 @@ class Index:
         
         # Reset things    
         self.im += 1
-        ax.clear()
+        ax_image.clear()
         reg_array[reg_array != 0] = 0
         
         # Draw the picture
         img = mpimg.imread(path + 'IM_'+ str(self.im) + '.jpg')
-        imgplot = ax.imshow(img, extent=[0, 50, 0, 50])
+        imgplot = ax_image.imshow(img, extent=[0, 50, 0, 50])
         
-        ax.set_xticks(x)
-        ax.set_yticks(x)
-        ax.grid(True)
+        ax_image.set_xticks(x)
+        ax_image.set_yticks(x)
+        ax_image.grid(True)
         
-        plt.draw()
+        ax_image.draw()
         
         # Visual reader
         if isinstance(wb['Interest_boxes'][self.im - 1],str) and '[' in wb['Interest_boxes'][self.im - 1]:
@@ -76,11 +70,11 @@ class Index:
                             # Background
                             rec_col = 'blue'
                             
-                        ax.add_patch(Rectangle((x_pos, y_pos), width=5, height=5, color=rec_col, alpha=0.2))  
-                        ax.figure.canvas.draw()
+                        ax_image.add_patch(Rectangle((x_pos, y_pos), width=5, height=5, color=rec_col, alpha=0.2))  
+                        ax_image.figure.canvas.draw()
                         
         
-        plt.title('IM_'+ str(self.im))
+        fig.suptitle('IM_'+ str(self.im))
         
         print("Now working on IM_" + str(self.im))
                 
@@ -89,7 +83,7 @@ class Index:
         self.im -= 1
 
         img = mpimg.imread(path + 'IM_'+ str(self.im) + '.jpg')
-        imgplot = ax.imshow(img, extent=[0, 50, 0, 50])
+        imgplot = ax_image.imshow(img, extent=[0, 50, 0, 50])
         plt.draw()
         
     def close(self, event):
@@ -99,8 +93,7 @@ class Index:
         plt.close('all')
         # Print out 
         print("Saved to Excel")
-
-   
+ 
    
 class Visual_annotator(object):
     def __init__(self):
@@ -156,14 +149,30 @@ class Visual_annotator(object):
             self.ax.add_patch(Rectangle((x_pos, y_pos), width=5, height=5, color=rec_col, alpha=0.2))  
             self.ax.figure.canvas.draw()                  
             
-       
-a = Visual_annotator()
 
-callback = Index()
+# Setting everything that has to be set
+base_path = "C:/Users/zmezl/Desktop/University/AI/Thesis/eXAI_thesis/"
+path = base_path + "Amin_dataset/"
+fig = plt.figure()
+grid = gridspec.GridSpec(3,1)
+
+ax_image = fig.add_subplot(grid[0])
+ax_buttons = fig.add_subplot(grid[1])
+ax_provoke = fig.add_subplot(grid[2])
+
+reg_array = np.zeros((10,10))
+wb = pd.read_excel(base_path + "fitzpatrick17k-amin-annotation.xlsx") 
+a = Visual_annotator()
+x = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+callback = Index()    
+
+initialize() 
+
+
 axprev = plt.axes([0.5, 0.05, 0.1, 0.075])
-axnext = plt.axes([0.61, 0.05, 0.2, 0.075])
+ax_next = plt.axes([0.61, 0.05, 0.2, 0.075])
 axclose = plt.axes([0.82, 0.05, 0.1, 0.075])
-bnext = Button(axnext, 'Next and save')
+bnext = Button(ax_next, 'Next and save')
 bnext.on_clicked(callback.next)
 bprev = Button(axprev, 'Previous')
 bprev.on_clicked(callback.prev)
